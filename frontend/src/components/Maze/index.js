d54dbb1cf924a37a219c4c17ecad8c8610f350ca
Thiago Maximo Pavão <thipavao44@gmail.com>
2023-01-08 19:24:51 -0300

    maze line caps

--- frontend/src/components/Maze/index.js
+++ frontend/src/components/Maze/index.js
@@ -4,9 +4,34 @@ import React from "react"
 import { BASE_SIZE, LARGE_LINE_WIDTH } from '../../settings/constants'
 
 const N = 1
-// const E = 2
+const E = 2
 const W = 4
-// const S = 8
+const S = 8
+
+const drawCellWalls = (g, maze, x, y, all = false) => {
+
+    if( (maze.grid[y][x] & N) === 0 ) {
+        g.moveTo(x * BASE_SIZE, y * BASE_SIZE)
+        g.lineTo((x+1) * BASE_SIZE, y * BASE_SIZE)
+    }
+
+    if( (maze.grid[y][x] & W) === 0 ) {
+        g.moveTo(x * BASE_SIZE, y * BASE_SIZE)
+        g.lineTo(x * BASE_SIZE, (y+1) * BASE_SIZE)
+    }
+
+    if(all) {
+        if( (maze.grid[y][x] & S) === 0 ) {
+            g.moveTo(x * BASE_SIZE, (y+1) * BASE_SIZE)
+            g.lineTo((x+1) * BASE_SIZE, (y+1) * BASE_SIZE)
+        }
+    
+        if( (maze.grid[y][x] & E) === 0 ) {
+            g.moveTo((x+1) * BASE_SIZE, y * BASE_SIZE)
+            g.lineTo((x+1) * BASE_SIZE, (y+1) * BASE_SIZE)
+        }
+    }
+}
 
 function Maze(props) {  
 
@@ -15,6 +40,7 @@ function Maze(props) {
     const draw = g => { // removed useCallback because map is only drawn once
 
         g.clear()
+
         g.lineStyle({width: LARGE_LINE_WIDTH, color: 0xffffff, alpha: 1, cap: PIXI.LINE_CAP.ROUND})
         
         g.moveTo(maze.width * BASE_SIZE, maze.height * BASE_SIZE)
@@ -25,46 +51,16 @@ function Maze(props) {
 
         for(let y = 0; y < maze.height; y++) {
             for(let x = 0; x < maze.width; x++) {
-
-                if( (maze.grid[y][x] & N) === 0 ) {
-                    if( maze.sx === x && (maze.sy === y || maze.sy === y-1) )      g.lineStyle(LARGE_LINE_WIDTH, 0x00ff00, 1)
-                    else if( maze.ex === x && (maze.ey === y || maze.ey === y-1) ) g.lineStyle(LARGE_LINE_WIDTH, 0xff0000, 1)
-                    else g.lineStyle(LARGE_LINE_WIDTH, 0xffffff, 1)
-    
-                    g.moveTo(x * BASE_SIZE, y * BASE_SIZE)
-                    g.lineTo((x+1) * BASE_SIZE, y * BASE_SIZE)
-                }
-
-                if( (maze.grid[y][x] & W) === 0 ) {
-                    if( (maze.sx === x || maze.sx === x-1) && maze.sy === y )      g.lineStyle(LARGE_LINE_WIDTH, 0x00ff00, 1)
-                    else if( (maze.ex === x || maze.ex === x-1) && maze.ey === y ) g.lineStyle(LARGE_LINE_WIDTH, 0xff0000, 1)
-                    else g.lineStyle(LARGE_LINE_WIDTH, 0xffffff, 1)
-
-                    g.moveTo(x * BASE_SIZE, y * BASE_SIZE)
-                    g.lineTo(x * BASE_SIZE, (y+1) * BASE_SIZE)
-                }
+                drawCellWalls(g, maze, x, y)
             }
         }
 
-        g.lineStyle(LARGE_LINE_WIDTH, 0x00ff00, 1)
-        if(maze.sx === maze.width-1) {
-            g.moveTo(maze.width * BASE_SIZE, maze.sy * BASE_SIZE)
-            g.lineTo(maze.width * BASE_SIZE, (maze.sy+1) * BASE_SIZE)
-        }
-        if(maze.sy === maze.height-1) {
-            g.moveTo(maze.sx * BASE_SIZE, maze.height * BASE_SIZE)
-            g.lineTo((maze.sx + 1) * BASE_SIZE, maze.height * BASE_SIZE)
-        }
+        g.lineStyle({width: LARGE_LINE_WIDTH, color: 0x00ff00, alpha: 1, cap: PIXI.LINE_CAP.ROUND})
+        drawCellWalls(g, maze, maze.sx, maze.sy, true)
 
-        g.lineStyle(LARGE_LINE_WIDTH, 0xff0000, 1)
-        if(maze.ex === maze.width-1) {
-            g.moveTo(maze.width * BASE_SIZE, maze.ey * BASE_SIZE)
-            g.lineTo(maze.width * BASE_SIZE, (maze.ey+1) * BASE_SIZE)
-        }
-        if(maze.ey === maze.height-1) {
-            g.moveTo(maze.ex * BASE_SIZE, maze.height * BASE_SIZE)
-            g.lineTo((maze.ex + 1) * BASE_SIZE, maze.height * BASE_SIZE)
-        }
+
+        g.lineStyle({width: LARGE_LINE_WIDTH, color: 0xff0000, alpha: 1, cap: PIXI.LINE_CAP.ROUND})
+        drawCellWalls(g, maze, maze.ex, maze.ey, true)
     }
     
 

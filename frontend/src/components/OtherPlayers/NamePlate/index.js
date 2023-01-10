50d483b1e8395726c864653b7ac716d6daae48a9
Thiago Maximo Pavão <thipavao44@gmail.com>
2023-01-10 20:38:09 -0300

    name plate visual improvement

--- frontend/src/components/OtherPlayers/NamePlate/index.js
+++ frontend/src/components/OtherPlayers/NamePlate/index.js
@@ -20,39 +20,23 @@ function NamePlate(props) {
     })
 
     let textMetrics = PIXI.TextMetrics.measureText(name, textStyle)
-    let verticalSpacing = textMetrics.height/2 + PLAYER_RADIUS/2 + 17
+    let verticalSpacing = textMetrics.height/2 + PLAYER_RADIUS/2 + 5
 
     const draw = useCallback(g => {
         let spacing = 10 // horizontal, to add more space between end of the text and end of NamePlate box.
-        let triangleHeight = 10
         let minWidth = 55
 
         let targetWidth = textMetrics.width + 2*spacing
         let rectWidth = targetWidth > minWidth ? targetWidth : minWidth
 
         g.clear()
-        g.beginFill(0x303030, 1)
-        g.lineStyle(LARGE_LINE_WIDTH,0,1)
+        g.beginFill(0x303030, 0.8)
+        g.lineStyle(LARGE_LINE_WIDTH,0,0)
 
 
-        g.drawRoundedRect(-rectWidth/2, -textMetrics.height/2, rectWidth, textMetrics.height, 10)
-        g.endFill()
-
-
-        g.lineStyle(LARGE_LINE_WIDTH,0x303030,1)
-        g.beginFill(0x303030);
-        g.moveTo(-triangleHeight, -textMetrics.height/2);
-        g.lineTo(0, -triangleHeight -textMetrics.height/2);
-        g.lineTo(+triangleHeight, -textMetrics.height/2);
-        g.lineTo(-triangleHeight, -textMetrics.height/2);
+        g.drawRoundedRect(-rectWidth/2, -textMetrics.height/2, rectWidth, textMetrics.height, 6)
         g.endFill()
         
-        g.lineStyle(LARGE_LINE_WIDTH,0,1)
-        g.moveTo(-triangleHeight-5, -textMetrics.height/2);
-        g.lineTo(-triangleHeight, -textMetrics.height/2);
-        g.lineTo(0, -triangleHeight -textMetrics.height/2);
-        g.lineTo(+triangleHeight, -textMetrics.height/2);
-        g.lineTo(+triangleHeight+5, -textMetrics.height/2);
     }, []);
 
     return(

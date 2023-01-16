88abd6a8a6ade354705ed7e82742ddd472a2a5fb
Thiago Maximo Pavão <thipavao44@gmail.com>
2023-01-16 13:49:45 -0300

    bug fix

--- frontend/src/components/ColorSelector/index.js
+++ frontend/src/components/ColorSelector/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 // styles
 import './index.css'
@@ -8,9 +8,12 @@ function ColorSelector({ onSelect }) {
     const [colors , setColors] = useState(['#FF0000', '#FF7F00', '#FFFF00', '#00FF00', '#0000FF', '#4B0082', '#9400D3'].reverse())
     const [selectedColor, setSelectedColor] = useState(colors[0]);
     
+    useEffect(() => {
+        onSelect(selectedColor);
+    }, [selectedColor]);
+
     const handleSelected = (color) => {
         setSelectedColor(color);
-        onSelect(color);
     }
 
     return (

5167ba670bee5018c39aaf161b6c53e6f649ecc8
Thiago Maximo Pavão <thipavao44@gmail.com>
2023-01-19 13:25:07 -0300

    "Go!" text added to end in counter

--- frontend/src/components/Counter/index.js
+++ frontend/src/components/Counter/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useCallback } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 
 import './style.css'
 
@@ -9,23 +9,38 @@ import '../../fonts/Inter/static/Inter-Regular.ttf'
 function Counter(props) {
     const initialTimer = props.time
 
+    const [intervalRef, setIntervalRef] = useState()
     const [time, setTime] = useState(Math.ceil(initialTimer/1000))
+
+    const counterUptadeRef = useRef();
+
+    useEffect(() => {
+        counterUptadeRef.current = () => {
+            if(time > 1) {
+                setTime(t => t-1)
+                return
+            }
+            setTime(t => t-1)
+            setTimeout(() => {
+                setTime(t => t-1)
+                clearInterval(intervalRef)
+            }, 333) // delay to clear "Go!" text
+        }
+    }, [time]);
 
     useEffect(() => {
         let interval
         setTimeout(() => {
             setTime(t => t-1)
-            interval = setInterval(() => {
-                setTime(t => t-1)
-            }, 1000)
+            interval = setInterval(() => counterUptadeRef.current(), 1000)
+            setIntervalRef(interval)
         }, initialTimer % 1000)
 
-        return () => clearInterval(interval)
+        return () => clearInterval(intervalRef)
     }, [])
 
-    if( time > 0 ) return (
-        <p className='counter-number'>{time}</p>
-    )
+    if( time > 0 ) return <p className='counter-number'>{time}</p>
+    else if(time === 0) return <p className='counter-number'>Go!</p>
     return null
 }
 

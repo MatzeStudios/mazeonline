c9b622462cad5841943080e552d11adbae7a996c
Thiago <thipavao44@gmail.com>
2023-06-04 18:20:04 -0300

    Novo import das fontes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,10 @@ import React from 'react'
 import './App.css'
 import Routes from './routes'
 
+// fonts
+import './fonts/Bungee_Shade/BungeeShade-Regular.ttf'
+import './fonts/Inter/static/Inter-Regular.ttf'
+
 function App() {
     return (
         <Routes />

e5be9824b2044a920ce97621340d4db4bbc6a045
Thiago <thipavao44@gmail.com>
2023-06-10 23:08:14 -0300

    using google fonts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,10 +3,6 @@ import React from 'react'
 import './App.css'
 import Routes from './routes'
 
-// fonts
-import './fonts/Bungee_Shade/BungeeShade-Regular.ttf'
-import './fonts/Inter/static/Inter-Regular.ttf'
-
 function App() {
     return (
         <Routes />
